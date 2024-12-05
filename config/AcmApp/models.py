a0c174eccff449241c317fda05122875fd4d82b7
Crazypandaz <rockmanjason@gmail.com>
2024-12-05 13:42:57 -0800

    - Changed graduation_time to CharField. - Switched to using emails as foreignKey instead of name for Class officers and comments. - added related_names for author and resume under Comments to avoid foreign key collision. - Added optional unique and null fields to appease DJango migrations.

--- config/AcmApp/models.py
+++ config/AcmApp/models.py
@@ -5,16 +5,16 @@ from django.db import models
 class Members(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
-    email = models.EmailField(max_length=200)
+    email = models.EmailField(max_length=200, unique=True)
     password = models.CharField(max_length=50)
     major = models.CharField(max_length=50, blank=True)
-    graduation_time = models.DateField(max_length=10, blank=True)
+    graduation_time = models.CharField(max_length=10, null=True)
     profile_photo_name = models.CharField(max_length=30, blank=True)
-    resume_file_name = models.CharField(max_length=30, blank=True)
+    resume_file_name = models.CharField(max_length=30, blank=True, unique=True)
 
 class Officers(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.OneToOneField(Members, on_delete=models.CASCADE, to_field='name')
+    email = models.OneToOneField(Members, on_delete=models.CASCADE, to_field='email')
     bio = models.TextField()
     position = models.CharField(max_length=50)
     responsibility = models.TextField()
@@ -23,7 +23,7 @@ class Events(models.Model):
     id = models.AutoField(primary_key = True)
     name = models.CharField(max_length= 50)
     scheduled_date_time = models.DateTimeField()
-    host = models.ManyToManyField(Members, to_field='name')
+    host = models.ManyToManyField(Members)
     sponsor = models.CharField(max_length=200, blank=True)
     rsvp = models.IntegerField(default=0)
 
@@ -31,8 +31,8 @@ class Comments(models.Model):
 
     id = models.AutoField(primary_key = True)
     content = models.TextField()
-    author = models.ForeignKey(Members, on_delete=models.CASCADE, to_field='name')
-    resume = models.ForeignKey(Members, on_delete=models.CASCADE, to_field='resume_file_name')
+    email = models.ForeignKey(Members, on_delete=models.CASCADE, to_field='email', related_name='email_of_comment')
+    resume = models.ForeignKey(Members, on_delete=models.CASCADE, to_field='resume_file_name', related_name='comment_on_resume')
 
 
 
