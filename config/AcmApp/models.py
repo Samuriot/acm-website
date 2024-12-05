aa890185617e22973c07f223cbbdb148fb02eeed
Crazypandaz <rockmanjason@gmail.com>
2024-12-05 11:07:43 -0800

    -implemented model for member, officer, event, and comment.

--- config/AcmApp/models.py
+++ config/AcmApp/models.py
@@ -3,10 +3,42 @@ from django.db import models
 # Create your models here.
 
 class Members(models.Model):
-    id = models.AutoField(primary_key = True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
-    major = models.CharField(max_length=50)
-    date_of_joining = models.DateField()
-    photo_file_name = models.CharField(max_length=50)
-    
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=50)
+    major = models.CharField(max_length=50, blank=True)
+    graduation_time = models.DateField(max_length=10, blank=True)
+    profile_photo_name = models.CharField(max_length=30, blank=True)
+    resume_file_name = models.CharField(max_length=30, blank=True)
+
+class Officers(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.OneToOneField(Members, on_delete=models.CASCADE, to_field='name')
+    bio = models.TextField()
+    position = models.CharField(max_length=50)
+    responsibility = models.TextField()
+
+class Events(models.Model):
+    id = models.AutoField(primary_key = True)
+    name = models.CharField(max_length= 50)
+    scheduled_date_time = models.DateTimeField()
+    host = models.ManyToManyField(Members, to_field='name')
+    sponsor = models.CharField(max_length=200, blank=True)
+    rsvp = models.IntegerField(default=0)
+
+class Comments(models.Model):
+
+    id = models.AutoField(primary_key = True)
+    content = models.TextField()
+    author = models.ForeignKey(Members, on_delete=models.CASCADE, to_field='name')
+    resume = models.ForeignKey(Members, on_delete=models.CASCADE, to_field='resume_file_name')
+
+
+
+
+
+
+
+
 
