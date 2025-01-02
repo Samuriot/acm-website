1b10f8ddb031d16bbeb7f116b0cb9f6912b79f81
Crazypandaz <rockmanjason@gmail.com>
2025-01-02 03:17:02 -0800

    -Updated MEDIA_URL and MEDIA_ROOT to 'media'

--- config/config/settings.py
+++ config/config/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 import os
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-MEDIA_URL = '/Photos/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "Photos")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
