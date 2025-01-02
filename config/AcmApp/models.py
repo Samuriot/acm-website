a36aebde9926ee748b9674d8586d844b5fd97fb3
Crazypandaz <rockmanjason@gmail.com>
2025-01-02 02:55:38 -0800

    -Modified photo and resume attribute to their respective field types. Added upload path to both as well. -Added __str__ functions for all models.

--- config/AcmApp/models.py
+++ config/AcmApp/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+def upload_to_image(instance, filename):
+    return '/'.join(['images', str(instance.email), filename])
+
+def upload_to_resume(instance, filename):
+    return '/'.join(['resumes', str(instance.email), filename])
 
 class Members(models.Model):
     id = models.AutoField(primary_key=True)
@@ -9,8 +16,13 @@ class Members(models.Model):
     password = models.CharField(max_length=50)
     major = models.CharField(max_length=50, blank=True)
     graduation_time = models.CharField(max_length=10, null=True)
-    profile_photo_name = models.CharField(max_length=30, blank=True)
-    resume_file_name = models.CharField(max_length=30, blank=True, unique=True)
+    photo = models.ImageField(upload_to=upload_to_image, null=True, blank=True, validators=[])
+    resume = models.FileField(upload_to=upload_to_resume, null=True, blank=True, validators=[], unique=True)
+
+    def __str__(self):
+        name = self.name
+        id = self.id
+        return f"Member {name=} {id=}"
 
 class Officers(models.Model):
     id = models.AutoField(primary_key=True)
@@ -18,6 +30,11 @@ class Officers(models.Model):
     bio = models.TextField()
     position = models.CharField(max_length=50)
     responsibility = models.TextField()
+
+    def __str__(self):
+        name = self.email
+        id = self.id
+        return f"Officer {email=} {id=}"
 
 class Events(models.Model):
     id = models.AutoField(primary_key = True)
@@ -27,13 +44,22 @@ class Events(models.Model):
     sponsor = models.CharField(max_length=200, blank=True)
     rsvp = models.IntegerField(default=0)
 
+    def __str__(self):
+        name = self.name
+        id = self.id
+        return f"Event {name=} {id=}"
+
 class Comments(models.Model):
 
     id = models.AutoField(primary_key = True)
     content = models.TextField()
     email = models.ForeignKey(Members, on_delete=models.CASCADE, to_field='email', related_name='email_of_comment')
-    resume = models.ForeignKey(Members, on_delete=models.CASCADE, to_field='resume_file_name', related_name='comment_on_resume')
+    resume = models.ForeignKey(Members, on_delete=models.CASCADE, to_field='resume', related_name='comment_on_resume')
 
+    def __str__(self):
+        email = self.email
+        id = self.id
+        return f"Member {email=} {id=}"
 
 
 
